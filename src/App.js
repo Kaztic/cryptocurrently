d718abb4d5935fb35284f7376be5fd66f06287f4
Daku1 <80529485+Daku1@users.noreply.github.com>
2021-05-08 11:53:51 +0530

    css change

--- src/App.js
+++ src/App.js
@@ -28,33 +28,37 @@ function App() {
   )
 
   return (
-    <div className="coin-app">
-      <div className="coin-search">
-        <h1 className="coin-text">Search a currency</h1>
-        <form>
-          <input
-            className="coin-input"
-            type="text"
-            onChange={handleChange}
-            placeholder="Search"
-          />
-        </form>
+    <>
+      <h1 className="cryptoCurrently">CryptoCurrently </h1>
+
+      <div className="coin-app">
+        <div className="coin-search">
+          <h1 className="coin-text">Search a currency</h1>
+          <form>
+            <input
+              className="coin-input"
+              type="text"
+              onChange={handleChange}
+              placeholder="Search"
+            />
+          </form>
+        </div>
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.current_price}
+              symbol={coin.symbol}
+              marketcap={coin.total_volume}
+              volume={coin.market_cap}
+              image={coin.image}
+              priceChange={coin.price_change_percentage_24h}
+            />
+          )
+        })}
       </div>
-      {filteredCoins.map((coin) => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            marketcap={coin.total_volume}
-            volume={coin.market_cap}
-            image={coin.image}
-            priceChange={coin.price_change_percentage_24h}
-          />
-        )
-      })}
-    </div>
+    </>
   )
 }
 
